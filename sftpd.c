2ab15ca3c44404e5b2e5c8a8ee8c00ea671bfd50
Mael Clerambault <mael@clerambault.fr>
2014-02-19 21:18:41 +0100

    Use fts functions instead of dirent functions and implement detailed listings

--- sftpd.c
+++ sftpd.c
@@ -3,9 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
-#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fts.h>
+#include <pwd.h>
+#include <grp.h>
 
 
 #define SSH_FXP_INIT                1
@@ -48,7 +53,12 @@
 #define SSH_FX_CONNECTION_LOST               7
 #define SSH_FX_OP_UNSUPPORTED                8
 
-
+#define SSH_FILEXFER_ATTR_SIZE          0x00000001
+#define SSH_FILEXFER_ATTR_UIDGID        0x00000002
+#define SSH_FILEXFER_ATTR_PERMISSIONS   0x00000004
+#define SSH_FILEXFER_ATTR_ACMODTIME     0x00000008
+#define SSH_FILEXFER_ATTR_EXTENDED      0x80000000
+    
 #define READ_DATA(data, length) do {\
   if((len_read = read(STDIN_FILENO, data, length)) == -1 || len_read == 0) \
     exit(-1); \
@@ -59,9 +69,10 @@
   READ_DATA(&data, sizeof(data)); \
 } while(0)
 
-#define READ_STRING(data) do {\
+#define READ_STRING(data) do { /* TODO : Check data boundaries */ \
   READ_VAR(str_length); \
   READ_DATA(data, ntohl(str_length)); \
+  data[ntohl(str_length)] = '\0'; \
 } while(0)
 
 #define WRITE_RESET() do {\
@@ -103,6 +114,49 @@
     WRITE(SSH_FXP_STATUS); \
 } while(0);
 
+#define GET_RIGHT_SYMBOL(st, r, v) ((st)->st_mode & (r) ? v : '-')
+
+char* ls_l(const char* name, const struct stat* st) {
+    static char buf[2048];
+    struct tm* mtime;
+    struct passwd* pw;
+    struct group* grp;
+    char file_type_symbol;
+    
+    switch(st->st_mode & S_IFMT) {
+        case S_IFIFO : file_type_symbol = 'p'; break;
+        case S_IFCHR : file_type_symbol = 'c'; break;
+        case S_IFDIR : file_type_symbol = 'd'; break;
+        case S_IFBLK : file_type_symbol = 'b'; break;
+        case S_IFLNK : file_type_symbol = 'l'; break;
+        case S_IFSOCK: file_type_symbol = 's'; break;
+        case S_IFREG : 
+        default:
+            file_type_symbol = '-';
+    }
+
+    mtime = localtime(&st->st_mtim.tv_sec);
+    pw = getpwuid(st->st_uid);
+    grp = getgrgid(st->st_gid);
+    
+    snprintf(buf, sizeof(buf), "%c%c%c%c%c%c%c%c%c%c %3u %8s %8s %8u %u-%02u-%02u % 2u:%02u %s",
+        file_type_symbol,
+        GET_RIGHT_SYMBOL(st, S_IRUSR, 'r'), GET_RIGHT_SYMBOL(st, S_IWUSR, 'w'), GET_RIGHT_SYMBOL(st, S_IXUSR, 'x'),
+        GET_RIGHT_SYMBOL(st, S_IRGRP, 'r'), GET_RIGHT_SYMBOL(st, S_IWGRP, 'w'), GET_RIGHT_SYMBOL(st, S_IXGRP, 'x'),
+        GET_RIGHT_SYMBOL(st, S_IROTH, 'r'), GET_RIGHT_SYMBOL(st, S_IWOTH, 'w'), GET_RIGHT_SYMBOL(st, S_IXOTH, 'x'),
+        st->st_nlink, pw->pw_name, grp->gr_name, st->st_size,
+        1900 + mtime->tm_year, mtime->tm_mon + 1, mtime->tm_mday, mtime->tm_hour, mtime->tm_min,
+        name
+    );
+
+    return buf;
+}
+
+#define WRITE_LS_L(name, stat) do {\
+    ls_l_str = ls_l(name, stat); \
+    WRITE_STR(ls_l_str, strlen(ls_l_str)); \
+} while(0)
+
 int main(int argc, char* argv[]) {
   uint32_t _msg_length_n, _msg_length;
   uint8_t _msg_type;
@@ -114,13 +168,21 @@ int main(int argc, char* argv[]) {
   uint32_t version;
   uint32_t id;
   uint32_t count;
+  
+  /* attr */
   uint32_t flags;
+  uint64_t size;
+  uint32_t uid, gid;
+  uint32_t permissions;
+  uint32_t atime, mtime;
+  
   uint32_t error_code;
-  int handle;
-  DIR* dir;
-  struct dirent* ent;
+  FTS* dir_handle;
+  FTSENT *ent;
   char in_buf[4096] = {0}, out_buf[4096] = {0};
+  char* ls_l_str;
   char* fxp_name_count_addr;
+  char* dir_path[2] = {0};
   
   WRITE_RESET();
   while((len_read = read(0, &in_length, sizeof(in_length))) != -1 && len_read != 0) {
@@ -157,38 +219,53 @@ int main(int argc, char* argv[]) {
         READ_VAR(id);
         
         READ_STRING(in_buf);
-        handle = open(in_buf, O_RDONLY);
+        dir_path[0] = in_buf;
+        dir_handle = fts_open(dir_path, FTS_PHYSICAL, NULL);
+        fts_read(dir_handle);
         
         WRITE_VAR(id);
-        WRITE_STR(&handle, sizeof(handle));
+        WRITE_STR(&dir_handle, sizeof(dir_handle));
         WRITE(SSH_FXP_HANDLE);
         break;
         
       case SSH_FXP_READDIR:
         READ_VAR(id);
         READ_VAR(str_length);
-        READ_DATA(&handle, ntohl(str_length)); /* Check length == sizeof(handle) */
+        READ_DATA(&dir_handle, ntohl(str_length)); /* TODO: Check length == sizeof(dir_handle) */
 
-        dir = fdopendir(handle);
-        
         count = 0;
         WRITE_VAR(id);
         fxp_name_count_addr = _msg_data + _msg_length_n;
         WRITE_VAR(count);
-        while(dir && (ent = readdir(dir))) {
-            if(!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, ".."))
-                continue;
+        while((ent = fts_read(dir_handle))) {
+            if(ent->fts_info == FTS_D)
+              fts_set(dir_handle, ent, FTS_SKIP);
+            if(ent->fts_info == FTS_DP)
+              continue;
             
             count++;
-            flags = 0;
+            flags = htonl(SSH_FILEXFER_ATTR_SIZE
+                    | SSH_FILEXFER_ATTR_UIDGID
+                    | SSH_FILEXFER_ATTR_PERMISSIONS
+                    | SSH_FILEXFER_ATTR_ACMODTIME
+                    );
+            size = htobe64(ent->fts_statp->st_size);
+            uid = htonl(ent->fts_statp->st_uid);
+            gid = htonl(ent->fts_statp->st_gid);
+            permissions = htonl(ent->fts_statp->st_mode);
+            atime = htonl(ent->fts_statp->st_atim.tv_sec);
+            mtime = htonl(ent->fts_statp->st_mtim.tv_sec);
             
-            WRITE_STR(ent->d_name, strlen(ent->d_name));
-            WRITE_STR(ent->d_name, strlen(ent->d_name));
+            WRITE_STR(ent->fts_name, ent->fts_namelen);
+            WRITE_LS_L(ent->fts_name, ent->fts_statp); /* TODO: check it is valid */
             WRITE_VAR(flags);
+            WRITE_VAR(size);
+            WRITE_VAR(uid); WRITE_VAR(gid);
+            WRITE_VAR(permissions);
+            WRITE_VAR(atime); WRITE_VAR(mtime);
         }
         
         if(count > 0) {
-            closedir(dir);
             count = htonl(count);
             memcpy(fxp_name_count_addr, &count, sizeof(count));
             WRITE(SSH_FXP_NAME);
@@ -201,9 +278,9 @@ int main(int argc, char* argv[]) {
       case SSH_FXP_CLOSE:
         READ_VAR(id);
         READ_VAR(str_length);
-        READ_DATA(&handle, ntohl(str_length)); /* Check length == sizeof(handle) */
+        READ_DATA(&dir_handle, ntohl(str_length)); /* TODO: Check length == sizeof(dir_handle) */
 
-        close(handle);
+        fts_close(dir_handle);
         
         WRITE_STATUS(id, SSH_FX_OK)
         break;
@@ -213,7 +290,7 @@ int main(int argc, char* argv[]) {
         exit(-1);
         break;      
     }
-    fprintf(stderr, "remaining bytes: %i\n", in_length);
+    //fprintf(stderr, "remaining bytes: %i\n", in_length);
     
   }
   
